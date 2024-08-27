ec0db82051bd59abda994b6de7cbf8c77a06bdb8
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-08-27 14:48:47 +0530

    Create Sign-Up api route

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  try {
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: "User Exists" });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/connectDb.js";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 
 connectDb(process.env.MONGO_URI);
+
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("App is listening on port 3000");
