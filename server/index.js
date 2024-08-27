c34daaff2b1296b8cd595d52bf12637670ff1725
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-08-27 14:57:25 +0530

    Creating Middleware for Error Handling

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log("App is listening on port 3000");
 });
