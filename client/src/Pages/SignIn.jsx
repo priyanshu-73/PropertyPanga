4f3b0c4351d839ec8389e9f4331bde8a9322af6c
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-08-27 19:47:23 +0530

    Created Login functionality

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  return <div></div>;
+  const [formData, setFormData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const res = await axios.post("/api/auth/signin", formData, {
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = res.data;
+      if (data.success === false) {
+        setIsLoading(false);
+        setError(data.message);
+        toast.error(data.message);
+        return;
+      }
+
+      setIsLoading(false);
+      setError(null);
+      navigate("/");
+      toast.success("Logged In Successfully!");
+    } catch (error) {
+      toast.error("Wrong Credentials!");
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign In
+        </button>
+      </form>
+      <div className="flex gap-2 mt-2">
+        <p>Dont have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700 hover:opacity-80">Sign-up</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
