c807dec61bf3a3fb1a8c1e25b21b88458669a749
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-09-02 12:49:01 +0530

    Update the header and make the profile protected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from "./Pages/About";
 import Profile from "./Pages/Profile";
 import Header from "./components/Header";
 import { Toaster } from "react-hot-toast";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -17,7 +18,9 @@ const App = () => {
         <Route path="sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
       <Toaster position="bottom-center" />
     </BrowserRouter>
