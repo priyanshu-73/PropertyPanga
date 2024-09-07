69f4dac37d5d017499e50dcef38960f6f1d66e20
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-09-07 23:11:07 +0530

    Create show listing api

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const updateUser = async (req, res, next) => {
@@ -40,6 +41,21 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
     res.status(200).json({ message: "User Deleted Successfully!" });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(
+      errorHandler("You must be logged in to view your listing!", 401)
+    );
+  }
+  try {
+    const listings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listings);
   } catch (error) {
     console.log(error);
     next(error);
