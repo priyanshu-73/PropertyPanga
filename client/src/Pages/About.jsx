b67e158cbbd2714f4ed7c4e1982a22b6987dc6ff
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-09-16 14:44:12 +0530

    Complete about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl font-semibold text-center p-9">
+        About Property Panga
+      </h1>
+      <p className="w-[80vw] text-center text-lg text-slate-600">
+        Welcome to Property Panga, your trusted partner in finding the perfect
+        home! We are a leading real estate platform designed to make buying,
+        renting, or selling properties as smooth and hassle-free as possible. At
+        Property Panga, we understand that finding the right property can be
+        overwhelming. That’s why we’ve built a user-friendly platform where you
+        can browse through a wide range of listings, from luxurious homes to
+        affordable rentals, all at your fingertips. Whether you’re looking for a
+        cozy apartment, a spacious family home, or a commercial space, we have
+        you covered. Our platform is more than just a listing service. With our
+        advanced filtering options, intuitive search functionality, and detailed
+        property descriptions, we empower you to make informed decisions. We
+        also provide comprehensive insights into market trends, property
+        valuations, and neighborhood information, ensuring you have all the data
+        you need to find the best deal. What makes us different? We believe in
+        personalized service. Our team of real estate experts is always
+        available to assist you with any queries, offering expert advice to help
+        you navigate the real estate market confidently. At Property Panga, our
+        mission is to connect you with your dream property quickly, efficiently,
+        and seamlessly. Your next home or investment is just a click away!
+      </p>
+    </div>
+  );
 };
 
 export default About;
